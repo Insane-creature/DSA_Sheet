b1f6da984c96934dc7397c4f119e9ce43225e582
Anshika Soni <anshikasoni357@gmail.com>
2023-11-28 23:39:07 +0530

    Finding single element

---
+++ Array/I/findingsingleelement.java
@@ -0,0 +1,19 @@
+public class findingsingleelement {
+        public static int getSingleElement(int []arr){
+            // Write your code here.
+             for (int i = 0; i < arr.length - 1; i+=2) {
+    
+                if (arr[i] != arr[i + 1])
+    
+                     return arr[i];
+    
+            }
+    
+            return arr[arr.length-1];
+        }
+    
+    public static void main(String[] args){
+        int[] arr = {1,1,2,2,3,4,4,5};
+        System.out.println(getSingleElement(arr));
+    }
+}
