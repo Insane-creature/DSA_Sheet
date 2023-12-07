16a933b307f358dbd58249c625d132c0a06a10dc
Anshika Soni <anshikasoni357@gmail.com>
2023-12-07 23:09:54 +0530

    adding file

---
+++ Array/II/maxsubarraysumk.java
@@ -0,0 +1,30 @@
+public class maxsubarraysumk {
+        public static int longestSubarrayWithSumK(int []a, long k) {
+            // Write your code here
+            int left = 0, right = 0;
+        long sum = a[0];
+        int maxLen = 0;
+        int n = a.length;
+        while(right<n){
+            while(left<=right && sum >k){
+                sum -= a[left];
+                left++;
+            }
+            if(sum ==k){
+                maxLen = Math.max(maxLen, right-left+1);
+            }
+            right++;
+            if(right<n) sum += a[right];
+
+        }
+        return maxLen;
+        }
+        public static void main(String[] args){
+            int[] a = {1, 2, 3, 1, 1,1,1};
+            int k = 3;
+            System.out.println(longestSubarrayWithSumK(a, k));
+
+        }
+
+
+}
