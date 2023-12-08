3a2f7672ef8c13006354298314e02d172401c167
Anshika Soni <anshikasoni357@gmail.com>
2023-12-09 00:17:20 +0530

    optimising

--- trial.java
+++ trial.java
@@ -3,13 +3,26 @@ import java.util.Scanner;
 public class trial {
     public static void main(String[] args) {
         // Bubble Sort
-        int[] arr = {14,9,15,12,6,8,13};
+        int[] arr = {3,1,-2,-5,2,-4};
         int n = arr.length;
 
-        
+        int[] nums = new int[n];
+        int posIndex = 0, negIndex = 1;
+
+        for(int i = 0; i<n;i++){
+            if(arr[i] < 0){
+                nums[negIndex] = arr[i];
+                negIndex +=2;
+                // System.out.println("The index is "+);
+            }
+            else{
+                nums[posIndex] = arr[i];
+                posIndex +=2;
+            }
+        }
         
         for(int i = 0; i<n;i++){
-            System.out.print(arr[i]+" ");
+            System.out.print(nums[i]+" ");
         }
 
     }
