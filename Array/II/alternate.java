02c3a2120c141ad05f54bc1060874ae9e23c8be7
Anshika Soni <anshikasoni357@gmail.com>
2023-12-11 22:57:58 +0530

    New code

--- Array/II/alternate.java
+++ Array/II/alternate.java
@@ -1,26 +1,29 @@
 public class alternate {
     public static void main(String[] args){
-        
-        // funtion
-        int[] arr = {3,1,-2,-5,2,-4};
-        int n = arr.length;
-        
-        int[] nums = new int[n];
-        int posIndex = 0, negIndex = 1;
-
-        for(int i = 0; i<n;i++){
-            if(arr[i] < 0){
-                nums[negIndex] = arr[i];
-                negIndex +=2;
-                // System.out.println("The index is "+);
-            }
-            else{
-                nums[posIndex] = arr[i];
-                posIndex +=2;
+        //
+        int[] nums = {3,1,-2,-5,2,-4};
+        int n = nums.length;
+        int arr1[] = new int[n/2];
+        int arr2[] = new int[n/2];
+        int j=0,k=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                arr1[j]=nums[i];
+                j++;
+            }else{
+                arr2[k]=nums[i];
+                k++;
             }
         }
         
-
+        j=0;k=0;
+        for(int i=0;i<n;i++){
+            nums[i] = arr2[k];
+            k++;
+            i++;
+            nums[i] = arr1[j];
+            j++;
+        }
         for(int i=0;i<n;i++){
             System.out.print(nums[i]+" ");
         }
