2cae04f0b3db1339c533d06c80ac6a445a4ba4bb
Anshika Soni <anshikasoni357@gmail.com>
2023-12-14 22:17:35 +0530

    Making changes -revision session

--- Learning Basics/Java/switch_case.java
+++ Learning Basics/Java/switch_case.java
@@ -25,5 +25,19 @@ public class switch_case {
 
         return result;
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ch = sc.nextInt();
+        double []a = new double[2];
+        if(ch == 2){
+            a[0] = sc.nextDouble();
+            a[1] = sc.nextDouble();
+        }
+        else{
+            a[0] = sc.nextDouble();
+        }
+        System.out.println(areaSwitchCase(ch, a));
+
+    }
 
 }
