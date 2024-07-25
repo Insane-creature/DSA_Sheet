a199e6a27a453924ae835c10573f14d96fa4dcd8
Anshika Soni <anshikasoni357@gmail.com>
2024-07-25 18:46:30 +0530

    staircase

--- Logic building/diagonalSum.py
+++ Logic building/diagonalSum.py
@@ -2,7 +2,6 @@ a = [[11, 2, 4],
      [4,5,6], 
      [10, 8, -12]]
 
-# print(a[2][1])    -> 8
 d1, d2 = 0, 0
 
 n = 3
@@ -10,8 +9,7 @@ for i in range(n):
     d1 += a[i][i]      
     d2 += a[i][n-i-1]  
 
-# print(d1)      # 4
-# print(d2)      # 19
-
 difference = d1-d2
+print(d1, d2)
+
 print(abs(difference))
---
+++ Logic building/staircase.py
@@ -0,0 +1,9 @@
+def staircase(n):
+    # Write your code here
+    
+
+    for i in range(1,n+1):
+        print((n-i)*' '+i*"#")
+
+n = 6
+staircase(n)
