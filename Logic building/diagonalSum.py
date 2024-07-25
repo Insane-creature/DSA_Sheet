df1a14fc18e4eec74db9bc2db4955951060399c3
Anshika Soni <anshikasoni357@gmail.com>
2024-07-25 18:04:23 +0530

    diagonal diff

--- Logic building/diagonalSum.py
+++ Logic building/diagonalSum.py
@@ -1,9 +1,17 @@
-a = [[1,2,3], 
+a = [[11, 2, 4],
      [4,5,6], 
-     [7,8,9]]
+     [10, 8, -12]]
 
 # print(a[2][1])    -> 8
+d1, d2 = 0, 0
 
 n = 3
 for i in range(n):
-    print(i+1)
+    d1 += a[i][i]      
+    d2 += a[i][n-i-1]  
+
+# print(d1)      # 4
+# print(d2)      # 19
+
+difference = d1-d2
+print(abs(difference))
